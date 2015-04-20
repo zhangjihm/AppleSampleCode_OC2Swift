f99d703255db9b79a8751773b3e09c982b7e4111
zhangjihm <zhangjihm@gmail.com>
2015-04-21 00:27:13 +0800

    locateMe

---
+++ LocateMe_Swift/LocateMe_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocateMe_Swift
+//
+//  Created by Bryan Zhang on 4/21/15.
+//  Copyright (c) 2015 Bryan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
